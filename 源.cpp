7ba50b360e50134484d2a1be3e4fcfc420cff923
chenwenshuo0308 <45116899+chenwenshuo0308@users.noreply.github.com>
2021-03-28 16:49:40 +0800

    Update 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -1,17 +1,8 @@
-#include<iostream>
-#include<typeinfo>
+#include <iostream>
 using namespace std;
-template <typename T>
-class My {
-public:
-	void f(const T&t) {
-		cout << typeid(t).name() << endl;
-	}
-};
-int main() {
-	My<int>obj1;
-	My < float>obj2;
-	obj1.f(2);
-	obj2.f(2);
-	return 0;
+ 
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
 }
